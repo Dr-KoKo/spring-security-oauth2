a503980caef635fab427eb4af6e2ee896f9f607f
Denny-Ko <donghar@naver.com>
2023-12-22 02:56:09 +0900

    Http Basic 인증

--- src/main/java/pe/goblin/springsecurityoauth2/SecurityConfig.java
+++ src/main/java/pe/goblin/springsecurityoauth2/SecurityConfig.java
@@ -1,18 +1,11 @@
 package pe.goblin.springsecurityoauth2;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-
-import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -20,21 +13,16 @@ public class SecurityConfig {
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 		httpSecurity
-			.formLogin(Customizer.withDefaults())
-			.httpBasic(Customizer.withDefaults());
+			.httpBasic(httpBasicConfigurer -> httpBasicConfigurer
+				.authenticationEntryPoint(new CustomAuthenticationEntryPoint()));
 
 		httpSecurity
-			.authorizeHttpRequests(customizer -> customizer
+			.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry
 				.anyRequest().authenticated());
 
-//		httpSecurity
-//			.exceptionHandling(customizer->customizer
-//				.authenticationEntryPoint(new AuthenticationEntryPoint() {
-//					@Override
-//					public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-//						System.out.println("custom entryPoint");
-//					}
-//				}));
+		httpSecurity
+			.sessionManagement(sessionManagementConfigurer -> sessionManagementConfigurer
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
 		return httpSecurity.build();
 	}
