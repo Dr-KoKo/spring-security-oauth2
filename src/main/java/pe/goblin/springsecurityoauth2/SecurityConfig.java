fff044fd57e51aec44d89c4d96846e4c89edd0b5
Denny-Ko <donghar@naver.com>
2023-12-21 13:50:37 +0900

    자동설정에 의한 초기화 과정 이해

--- src/main/java/pe/goblin/springsecurityoauth2/SecurityConfig.java
+++ src/main/java/pe/goblin/springsecurityoauth2/SecurityConfig.java
@@ -4,17 +4,29 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 	@Bean
-	SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+	SecurityFilterChain securityFilterChain1(HttpSecurity httpSecurity) throws Exception {
 		httpSecurity
 			.authorizeHttpRequests(customizer -> customizer
 				.anyRequest().authenticated())
 			.formLogin(Customizer.withDefaults())
 			.apply(new CustomSecurityConfigurer().setSecure(false));
+
+		return httpSecurity.build();
+	}
+
+	@Bean
+	SecurityFilterChain securityFilterChain2(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity
+			.authorizeHttpRequests(customizer -> customizer
+				.anyRequest().authenticated())
+			.httpBasic(Customizer.withDefaults());
 
 		return httpSecurity.build();
 	}
