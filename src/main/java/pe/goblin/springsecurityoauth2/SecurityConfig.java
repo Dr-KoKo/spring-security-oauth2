188aaa1817c3952c32fe6324a929222fcfcdc7c5
Denny-Ko <donghar@naver.com>
2023-12-21 16:44:29 +0900

    AuthenticationEntryPoint 이해

--- src/main/java/pe/goblin/springsecurityoauth2/SecurityConfig.java
+++ src/main/java/pe/goblin/springsecurityoauth2/SecurityConfig.java
@@ -1,32 +1,40 @@
 package pe.goblin.springsecurityoauth2;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 	@Bean
-	SecurityFilterChain securityFilterChain1(HttpSecurity httpSecurity) throws Exception {
+	SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 		httpSecurity
-			.authorizeHttpRequests(customizer -> customizer
-				.anyRequest().authenticated())
 			.formLogin(Customizer.withDefaults())
-			.apply(new CustomSecurityConfigurer().setSecure(false));
+			.httpBasic(Customizer.withDefaults());
 
-		return httpSecurity.build();
-	}
-
-	@Bean
-	SecurityFilterChain securityFilterChain2(HttpSecurity httpSecurity) throws Exception {
 		httpSecurity
 			.authorizeHttpRequests(customizer -> customizer
-				.anyRequest().authenticated())
-			.httpBasic(Customizer.withDefaults());
+				.anyRequest().authenticated());
+
+//		httpSecurity
+//			.exceptionHandling(customizer->customizer
+//				.authenticationEntryPoint(new AuthenticationEntryPoint() {
+//					@Override
+//					public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+//						System.out.println("custom entryPoint");
+//					}
+//				}));
 
 		return httpSecurity.build();
 	}
