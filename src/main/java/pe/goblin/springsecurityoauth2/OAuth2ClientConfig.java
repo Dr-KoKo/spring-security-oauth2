db9a7f3223def664b815ca05f4b4ee53b0b7eef6
Denny-Ko <donghar@naver.com>
2023-12-24 17:55:10 +0900

    OAuth2LoginConfigurer 초기화 이해

---
+++ src/main/java/pe/goblin/springsecurityoauth2/OAuth2ClientConfig.java
@@ -0,0 +1,23 @@
+package pe.goblin.springsecurityoauth2;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class OAuth2ClientConfig {
+
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity
+			.authorizeHttpRequests(AuthorizationManagerRequestMatcherRegistry -> AuthorizationManagerRequestMatcherRegistry
+				.anyRequest().authenticated())
+			.oauth2Login(Customizer.withDefaults());
+
+		return httpSecurity.build();
+	}
+}
