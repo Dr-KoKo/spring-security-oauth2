6fdca8bdb462b0531f4c77c329182e3576955cee
Denny-Ko <donghar@naver.com>
2023-12-25 00:13:37 +0900

    OAuth2 로그인 구현 - Authorization Code 요청하기

--- src/main/java/pe/goblin/springsecurityoauth2/OAuth2ClientConfig.java
+++ src/main/java/pe/goblin/springsecurityoauth2/OAuth2ClientConfig.java
@@ -1,26 +1,8 @@
 package pe.goblin.springsecurityoauth2;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
 @EnableWebSecurity
 public class OAuth2ClientConfig {
 
-	@Bean
-	SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-		httpSecurity
-			.authorizeHttpRequests(AuthorizationManagerRequestMatcherRegistry -> AuthorizationManagerRequestMatcherRegistry
-//				.requestMatchers("/loginPage").permitAll()
-				.anyRequest().authenticated())
-			.oauth2Login(Customizer.withDefaults());
-//			.oauth2Login(OAuth2LoginConfigurer->OAuth2LoginConfigurer
-//				.loginPage("/loginPage"));
-
-		return httpSecurity.build();
-	}
 }
