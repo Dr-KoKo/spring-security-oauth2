e09f0a31952bc1ecef1897d141a772facf026083
Denny-Ko <donghar@naver.com>
2023-12-25 18:46:52 +0900

    OAuth2 로그인 구현 - OAuth 2.0 User 모델

--- src/main/java/pe/goblin/springsecurityoauth2/OAuth2ClientConfig.java
+++ src/main/java/pe/goblin/springsecurityoauth2/OAuth2ClientConfig.java
@@ -1,8 +1,24 @@
 package pe.goblin.springsecurityoauth2;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
 public class OAuth2ClientConfig {
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity
+			.authorizeHttpRequests(AuthorizationManagerRequestMatcherRegistry -> AuthorizationManagerRequestMatcherRegistry
+				.anyRequest().permitAll());
 
+		httpSecurity
+			.oauth2Login(Customizer.withDefaults());
+
+		return httpSecurity.build();
+	}
 }
